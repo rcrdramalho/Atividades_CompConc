7de02ba882a32ab493b21bed7a00d2e0693fe8bb
Ricardo Ramalho <ricado.ramalho2@gmail.com>
2024-09-12 15:21:33 -0300

    Add files via upload

---
+++ lab5/soma-lock-atom.c
@@ -0,0 +1,98 @@
+/* Disciplina: Programacao Concorrente */
+/* Prof.: Silvana Rossetto */
+/* Codigo: Comunicação entre threads usando variável compartilhada e exclusao mutua com bloqueio */
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <pthread.h>
+
+long int soma = 0; //variavel compartilhada entre as threads
+pthread_mutex_t mutex; //variavel de lock para exclusao mutua
+pthread_cond_t cond;
+pthread_cond_t cond_2;
+int contador;
+int nthreads; //qtde de threads (passada linha de comando)
+
+//funcao executada pelas threads
+void *ExecutaTarefa (void *arg) {
+  long int id = (long int) arg;
+  printf("Thread : %ld esta executando...\n", id);
+
+  for (int i=0; i<100000; i++) {
+    //--entrada na SC
+    pthread_mutex_lock(&mutex);
+    //--SC (seção critica)
+    if(!(soma%10)){
+      if(contador<20){
+        pthread_cond_wait(&cond,&mutex);
+        pthread_cond_signal(&cond_2);
+      }
+    }
+    soma++;
+    //--saida da SC
+    pthread_mutex_unlock(&mutex);
+  }
+  printf("Thread : %ld terminou!\n", id);
+  pthread_exit(NULL);
+}
+
+//funcao executada pela thread de log
+void *extra (void *args) {
+  printf("Extra : esta executando...\n");
+  while(contador<20){
+     if (!(soma%10)) //imprime se 'soma' for multiplo de 10
+        printf("soma = %ld \n", soma);
+        contador++;
+        pthread_cond_signal(&cond);
+        pthread_cond_wait(&cond_2,&mutex);
+  }
+  printf("Extra : terminou!\n");
+  pthread_exit(NULL);
+}
+
+//fluxo principal
+int main(int argc, char *argv[]) {
+   pthread_t *tid; //identificadores das threads no sistema
+
+   //--le e avalia os parametros de entrada
+   if(argc<2) {
+      printf("Digite: %s <numero de threads>\n", argv[0]);
+      return 1;
+   }
+   nthreads = atoi(argv[1]);
+
+   //--aloca as estruturas
+   tid = (pthread_t*) malloc(sizeof(pthread_t)*(nthreads+1));
+   if(tid==NULL) {puts("ERRO--malloc"); return 2;}
+
+   //--inicilaiza o mutex (lock de exclusao mutua)
+   pthread_mutex_init(&mutex, NULL);
+   pthread_cond_init(&cond,NULL);
+   pthread_cond_init(&cond_2,NULL);
+
+   //--cria as threads
+   for(long int t=0; t<nthreads; t++) {
+     if (pthread_create(&tid[t], NULL, ExecutaTarefa, (void *)t)) {
+       printf("--ERRO: pthread_create()\n"); exit(-1);
+     }
+   }
+
+   //--cria thread de log
+   if (pthread_create(&tid[nthreads], NULL, extra, NULL)) {
+      printf("--ERRO: pthread_create()\n"); exit(-1);
+   }
+
+   //--espera todas as threads terminarem
+   for (int t=0; t<nthreads+1; t++) {
+     if (pthread_join(tid[t], NULL)) {
+         printf("--ERRO: pthread_join() \n"); exit(-1); 
+     } 
+   } 
+
+   //--finaliza o mutex
+   pthread_mutex_destroy(&mutex);
+   
+   printf("Valor de 'soma' = %ld\n", soma);
+
+   return 0;
+}
